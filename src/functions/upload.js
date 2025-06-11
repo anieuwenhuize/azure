5bb7a2f2f89efe6b8229f35d894ce626b0f7c6a6
Andries Nieuwenhuize <nieu0082@hz.nl>
2025-06-11 13:47:32 +0200

    counting number of blobs in blebber

--- src/functions/upload.js
+++ src/functions/upload.js
@@ -1,13 +1,27 @@
-const { app } = require('@azure/functions');
+import { app } from '@azure/functions';
+import { BlobServiceClient } from '@azure/storage-blob';
 
 app.http('upload', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
+
+        // Connect to Azure Blob Storage
+        const connectionString = "DefaultEndpointsProtocol=https;AccountName=cemtented;AccountKey=8zozpuVwCjpTu51pMnx4DLpTFnmu2tGRJ+OcHmFJNBGZ5mcF5pPzrjwR1Wvp119QLUwoUlzSkFZh+ASthwtBwQ==;EndpointSuffix=core.windows.net";
+        const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+        const containerClient = blobServiceClient.getContainerClient("abc");
+
         context.log(`Http function processed request for url "${request.url}"`);
+
+        const blobs = await containerClient.listBlobsFlat();
+        
+        // determine number of blobbies
+        const blebber = 0;
+        for await (const blob of blobs)
+            blebber++;
 
         const name = request.query.get('name') || await request.text() || 'world';
 
-        return { body: `Hello, ${name}!` };
+        return { body: `Hello, ${name}! There are number of ${blebber} previous requerst.` };
     }
 });
